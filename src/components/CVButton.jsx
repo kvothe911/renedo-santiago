70639a3d86bf0898016464e40b41fa7e5153a876
Santiago Renedo <kvothe911@gmail.com>
2024-08-27 01:50:31 +0200

    agregado boton de cv y cv.pdf a public

---
+++ src/components/CVButton.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import { useTranslation } from "react-i18next";
+
+function CVButton() {
+  const { t } = useTranslation();
+  const [pdfUrl] = useState('/renedo-santiago/Renedo-Santiago-CV.pdf'); // Ruta del PDF en la carpeta public
+
+  const handleOpenPDF = () => {
+    // Abre el PDF en una nueva pestaña
+    window.open(pdfUrl, '_blank');
+  };
+
+  return (
+    <div className="relative pt-2">
+      <button
+        onClick={handleOpenPDF}
+        className="bg-lime-400  hover:shadow-md hover:shadow-lime-900 hover:scale-[105%] text-black font-bold mt-4 lg:-mt-2 py-2 px-4 rounded duration-300 move"
+      >
+        {t("cv-button")}
+      </button>
+    </div>
+  );
+}
+
+export default CVButton;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,15 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { FaGithub, FaInstagram, FaLinkedinIn } from "react-icons/fa";
 import { FaArrowDownLong } from "react-icons/fa6";
 import "../styles.css";
 import { useTranslation } from "react-i18next";
-import "../i18n"
+import "../i18n";
 import RandomTextEffect from "./RandomTextEffect";
 import { useAnimation } from "./AnimationContext";
+import CVButton from "./CVButton";
 
 function Main() {
-
-    const { t } = useTranslation();
+  const { t } = useTranslation();
   const { triggerAnimation } = useAnimation(); // Obtén el triggerAnimation del contexto
   const [isHovered, setIsHovered] = useState(false);
   const [animationTimeout, setAnimationTimeout] = useState(null); // Manejo del timeout
@@ -29,7 +29,8 @@ function Main() {
   }, [triggerAnimation]);
 
   const handleMouseEnter = () => {
-    if (!isHovered) { // Solo activa el hover si no está activado
+    if (!isHovered) {
+      // Solo activa el hover si no está activado
       setIsHovered(true);
       const timeout = setTimeout(() => {
         setIsHovered(false); // Desactiva el hover después de 2 segundos
@@ -37,7 +38,6 @@ function Main() {
       setAnimationTimeout(timeout); // Guarda el timeout para limpiar más tarde
     }
   };
-    
 
   return (
     <div id="main" className="bg-black">
@@ -45,54 +45,61 @@ function Main() {
       <div className="absolute top-0 left-0 w-full h-full gradient-bottom-to-transparent"></div>
       <div className="w-full h-screen absolute top-0 left-0">
         <div className="max-w-[880px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center">
-        <p
-            className="text-neutral-100 sm:text-3xl text-xl"
-          >
-            <RandomTextEffect text={t('welcome')} triggerAnimation={triggerAnimation} />
+          <p className="text-neutral-100 sm:text-3xl text-xl">
+            <RandomTextEffect
+              text={t("welcome")}
+              triggerAnimation={triggerAnimation}
+            />
           </p>
           <h1
             className="text-lime-400 text-3xl sm:text-5xl font-bold pt-6"
             onMouseEnter={handleMouseEnter}
           >
-            <RandomTextEffect text="<santiago renedo>" triggerAnimation={isHovered} />
+            <RandomTextEffect
+              text="<santiago renedo>"
+              triggerAnimation={isHovered}
+            />
           </h1>
-          <h2
-            className="text-neutral-100 text-xl sm:text-3xl pt-8"
-          >
-            <RandomTextEffect text={t('description')} triggerAnimation={triggerAnimation} />
+          <h2 className="text-neutral-100 text-xl sm:text-3xl pt-8">
+            <RandomTextEffect
+              text={t("description")}
+              triggerAnimation={triggerAnimation}
+            />
           </h2>
-          <div className="flex gap-6 pt-10">
-            
-            <a
-              href="https://www.linkedin.com/in/santiago-renedo-527752303/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaLinkedinIn
-                size={25}
-                className="text-neutral-100 cursor-pointer hover:scale-[115%] hover:text-lime-400 duration-300 move"
-              />
-            </a>
-            <a
-              href="https://github.com/kvothe911"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaGithub
-                size={25}
-                className="text-neutral-100 cursor-pointer hover:scale-[115%] hover:text-lime-400 duration-300 move"
-              />
-            </a>
-            <a
-              href="https://www.instagram.com/santiago_renedo/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaInstagram
-                size={25}
-                className="text-neutral-100 cursor-pointer hover:scale-[115%] hover:text-lime-400 duration-300 move"
-              />
-            </a>
+          <div className="flex flex-col lg:flex-row lg:gap-10 gap-6 pt-10 m-0 items-center">
+            <div className="flex gap-6">
+              <a
+                href="https://www.linkedin.com/in/santiago-renedo-527752303/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaLinkedinIn
+                  size={25}
+                  className="text-neutral-100 cursor-pointer hover:scale-110 hover:text-lime-400 transition-transform duration-300"
+                />
+              </a>
+              <a
+                href="https://github.com/kvothe911"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaGithub
+                  size={25}
+                  className="text-neutral-100 cursor-pointer hover:scale-110 hover:text-lime-400 transition-transform duration-300"
+                />
+              </a>
+              <a
+                href="https://www.instagram.com/santiago_renedo/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaInstagram
+                  size={25}
+                  className="text-neutral-100 cursor-pointer hover:scale-110 hover:text-lime-400 transition-transform duration-300"
+                />
+              </a>
+            </div>
+            <CVButton />
           </div>
         </div>
       </div>
