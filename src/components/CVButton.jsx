70a97fc8e810b0abb0f126c5b4ae7dbec9d97922
Santiago Renedo <kvothe911@gmail.com>
2024-08-27 22:44:29 +0200

    edicion boton del cv para que tenga tamaño fijo y actualizacion del cv pdf

--- src/components/CVButton.jsx
+++ src/components/CVButton.jsx
@@ -13,11 +13,12 @@ function CVButton() {
   return (
     <div className="relative pt-2">
       <button
-        onClick={handleOpenPDF}
-        className="bg-lime-400  hover:shadow-md hover:shadow-lime-900 hover:scale-[105%] text-black font-bold mt-4 lg:-mt-2 py-2 px-4 rounded duration-300 move"
-      >
-        {t("cv-button")}
-      </button>
+  onClick={handleOpenPDF}
+  className="bg-lime-400 hover:shadow-md hover:shadow-lime-900 hover:scale-[105%] text-black font-bold mt-4 lg:-mt-2 py-2 px-4 rounded duration-300 move transition-all ease-in-out"
+  style={{ minWidth: '138px' }}
+>
+  {t("cv-button")}
+</button>
     </div>
   );
 }
